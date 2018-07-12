87e800432cc2be650d3017b0b89bafb8159da17d
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2018-07-11 21:22:34 -0400

    state updater

--- src/Draft/state.js
+++ src/Draft/state.js
@@ -1,17 +1,47 @@
 var redis = require("redis")
 
 const newDraft = (players, captains, admins) => {
+  const client = redis.createClient();
+
+  client.on("error", function (err) {
+    console.log("Error " + err);
+  });
+
   state = {
     players: players,
     teams: captains.map((cap) => {
       return {
         name: `${cap.name}'s Team`,
         captain: cap,
+        players: [],
         points: 0
       }
     }),
     admins: admins,
     transactions: []
   }
-  return state
+
+  const playerArray = ['players']
+  players.forEach((p)=> {
+    playerArray.push(p.ELO)
+    playerArray.push(JSON.stringify(p))
+  })
+  client.zadd(playerArray)
+
+  const teamArray = ['teams']
+  captains.forEach((cap) => {
+    teamArray.push(cap.points)
+    teamArray.push(JSON.stringify({
+      name: `${cap.name}'s Team`,
+      captain: cap,
+      players: [],
+      points: 0
+    }))
+  })
+  client.zadd(teamArray)
+  return client
+}
+
+const getCurrentState = (client) => {
+
 }
