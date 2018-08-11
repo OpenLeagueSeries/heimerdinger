2868b66f456d227358f11878dddd36dc80099316
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2018-08-11 04:48:47 -0400

    some cleanup after rebase

--- src/Draft/index.js
+++ src/Draft/index.js
@@ -8,36 +8,6 @@ import http2 from 'http2';
  * Bid transactions are merged into the assignment transaction after a bid is closed
  *
  * Each player bid has its own central timer that may be held by either admins or captains
-<<<<<<< HEAD
-*/
-
-/*
- * current bid state - hashmap
- * player to be bid on list - sorted set
- * bidders list - set (for scanning)
-*/
-
-const Subscribers = new Set();
-
-const draftHandler = (stream, headers, body) => {
-
-  if (body.length === 0) {
-    stream.write(JSON.stringify({number: 34}));
-    Subscribers.has(stream) || Subscribers.add(stream);
-  } else {
-    console.log(Subscribers.size)
-    Subscribers.forEach((sub) => {
-      sub.write(JSON.stringify({number: (body.number%2 === 1 ? 3 * body.number + 1: body.number/2)}))
-    })
-  }
-  stream.on('error', (e) => {
-    console.log(e);
-  })
-  stream.on('close', () => {
-    Subscribers.delete(stream);
-  })
-
-=======
 */
 
 /*
@@ -63,15 +33,6 @@ const draftHandler = (stream, body, user) => {
   stream.on('close', () => {
     Subscribers.delete(stream)
   })
-
-<<<<<<< HEAD
-  stream.write(JSON.stringify({array:['a']}))
-  setTimeout(function () {
-    stream.write(JSON.stringify({array:['a', 'b','c']}))
-  }, 2000);
->>>>>>> working stream stuff
-=======
->>>>>>> fixed a thing quick
 }
 
 export default draftHandler
