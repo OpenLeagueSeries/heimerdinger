55de000756d2aa185c6dd77385de6b417d446893
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2018-08-19 18:17:57 -0400

    wrote a subscription wrapper thing

---
+++ src/lib.js
@@ -0,0 +1,25 @@
+export default class SubscriptionWrapper {
+  constructor() {
+    const Subscribers = new Set();
+  }
+
+  sub(subscriber, data) {
+    subscriber.write(JSON.stringify(data));
+    return Subscribers.has(stream) || Subscribers.add(stream);
+  }
+
+  unsub(subscriber) {
+    Subscribers.delete(subscriber);
+
+    if (Subscribers.length === 0) {
+      return false;
+    }
+    return true;
+  }
+
+  update(data) {
+    Subscribers.forEach((sub) => {
+      sub.write(JSON.stringify(data));
+    })
+  }
+}
