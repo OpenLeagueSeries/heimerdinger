94ad524cc43f2f4c79c5ea9a77c60f37d795cf2e
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2018-08-20 20:56:48 -0400

    setup

--- src/User/index.js
+++ src/User/index.js
@@ -4,13 +4,16 @@ import SubscriptionWrapper from '../lib.js';
 
 const userSub = new SubscriptionWrapper();
 export const userHandler = (stream, user) => {
-  stream.write(JSON.stringify({userList: [
+  userSub.sub(stream, {userList: [
     {id: 1, name: 'Michael Santana', ign: 'imaqtpie', isCurrentPlayer: true, notes:'', roles: 'ADC', cap_in: false},
     {id: 2, name: 'Zacqueri Black', ign: 'Aphromoo', isCurrentPlayer: false, notes:'', roles: 'Support', cap_in: true},
     {id: 3, name: 'Peng Yiliang', ign: 'Doublelift', isCurrentPlayer: false, notes:'', roles: 'ADC', cap_in: false},
     {id: 4, name: 'Søren Bjerg', ign: 'Bjergsen', isCurrentPlayer: false, notes:'', roles: 'Mid', cap_in: true},
     {id: 5, name: 'Trevor Hayes', ign: 'Stixxay', isCurrentPlayer: false, notes:'', roles: 'ADC', cap_in: false}
-  ]}))
+  ]})
+  stream.on('end', () => (
+    userSub.unsub(stream);
+  ))
 }
 
 export const registerHandler = (stream, body, user) => {
@@ -19,7 +22,7 @@ export const registerHandler = (stream, body, user) => {
       bindVars: { name: body.name, email: body.email}
   })
   .then((result) => {
-
+    userSub.update(result)
   })
   .catch((e)=> {
     console.log(e)
