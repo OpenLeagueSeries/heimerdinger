6e9901f4a2e455a5b733310da4b36293443de6b0
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2018-08-23 18:51:54 -0400

    fixed some old issues

--- src/User/index.js
+++ src/User/index.js
@@ -1,13 +1,14 @@
 import http2 from 'http2';
 import db from '../DB/index.js';
+import { aql } from 'arangojs';
 import SubscriptionWrapper from '../lib.js';
 
 const userSub = new SubscriptionWrapper();
 
 export const userHandler = (stream, user) => {
-  userSub.sub(stream, db.query({
-    query: "FOR u IN User RETURN u"
-  }));
+  userSub.sub(stream,
+    db.query(aql`FOR u IN User RETURN u`)
+    .then((arangoResponse)));
 }
 
 export const registerHandler = (stream, body, user) => {
