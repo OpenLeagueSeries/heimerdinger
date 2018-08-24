bcc625f917af1e4d8092297257b7c4517b447c45
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2018-08-23 20:42:18 -0400

    ign query

--- src/Details/index.js
+++ src/Details/index.js
@@ -8,8 +8,9 @@ const userDetailsSub = new SubscriptionWrapper();
 export const detailsHandler = (stream, user, path) => {
   userDetailsSub.sub(stream,
     db.query(aql`FOR u IN User
-                 FILTER u.ign == `+path[0]+`
-                RETURN u`)
+                 FILTER u.ign == @ign
+                RETURN u`,
+              {ign: path[0]})
     .then((arangoResponse) => {
       return arangoResponse._result;
     }));
