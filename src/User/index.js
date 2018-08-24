e2e23ded5fe38eb3061a7ec34d91bed7f11fdc95
jaredclin <jaredc0307@gmail.com>
2018-08-23 22:04:21 -0400

    mailgun feature (#9)
    Add Mailgun api call, key is placeholder

--- src/User/index.js
+++ src/User/index.js
@@ -2,8 +2,10 @@ import http2 from 'http2';
 import db from '../DB/index.js';
 import { aql } from 'arangojs';
 import SubscriptionWrapper from '../lib.js';
+import createMailgun from 'mailgun-js';
 
 const userSub = new SubscriptionWrapper();
+const mg = createMailgun({apiKey: 'fornicatebrentius', domain: 'mg.pitt.lol'});
 
 export const userHandler = (stream, user) => {
   userSub.sub(stream,
@@ -18,7 +20,20 @@ export const registerHandler = (stream, body, user) => {
   userSub.update(db.query({
         query: "INSERT {'name' : @name,'email' : @email, 'ign' : @ign} INTO User",
         bindVars: { name: body.name, email: body.email, ign: body.ign}
-  }).catch((err)=> {
+  }).then((result)=>{
+		const data = {
+			from: 'LoL @ Pitt <lolatpitt@mg.pitt.lol>',
+			to: body.email,
+			subject: 'LoL@Pitt Registration',
+			text: `Hello, ${body.name}. Thank you for registering to play in LoL@Pitt's OLS Tournament this fall. Please go to this link in order to complete your signup: www.fish4hoes.com`
+		};
+ console.log(mg);
+		mg.messages().send(data, function (error, response) {
+      console.log(error);
+      console.log(response);
+  	});
+  })
+	  .catch((err)=> {
     console.log(err)
   }))
 
