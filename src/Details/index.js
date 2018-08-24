6ef56fe6371cebc630a1540b159230ae02f66ed4
zvirshup <zvirshup@gmail.com>
2018-08-23 20:54:04 -0400

    more syntax problems, path not containing all parts

--- src/Details/index.js
+++ src/Details/index.js
@@ -6,6 +6,7 @@ import SubscriptionWrapper from '../lib.js';
 const userDetailsSub = new SubscriptionWrapper();
 
 export const detailsHandler = (stream, user, path) => {
+  console.log(path);
   userDetailsSub.sub(stream,
     db.query(aql`FOR u IN User
                  FILTER u.ign == @ign
